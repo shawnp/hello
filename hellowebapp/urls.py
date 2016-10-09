c209827bceb5a066d1566c77fe9dd5d984c3dc17
Home_laptop <shawn.philbert@gmail.com>
2016-10-09 10:39:01 -0500

    added NAV

--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from collection import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
 	url(r'^$', views.index, name='home'),
+	url(r'^about/$', TemplateView.as_view(template_name='about.html'),name='about'),
+	url(r'^contact/$', TemplateView.as_view(template_name='contact.html'),name='contact'),
     url(r'^admin/', admin.site.urls),
 ]
