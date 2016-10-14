d3f8950a2c309601ed3d07f6abfddb566be76370
Home_laptop <shawn.philbert@gmail.com>
2016-10-13 21:30:40 -0500

    added filters

--- collection/views.py
+++ collection/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+# def index(request):
+# 	#defining a variable
+# 	number = 9
+# 	thing = "Thing name"
+# 	#this is our new def def view
+# 	return render(request, 'index.html', {'number':number,})
+
 def index(request):
-	#this is our new def def view
-	return render(request, 'index.html')
+	number = 6
+	# Dont forget the quotes because its a string,
+	# Not an interger
+	thing = "shawn philbert"
+	return render(request, 'index.html',{'number':number,
+		#Donot forget to pass it in tand the last comma
+		'thing':thing,
+	})
