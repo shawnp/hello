2b2dbad241f6a3c7de8b472e9cb3fbd6b467ea96
Home_laptop <shawn.philbert@gmail.com>
2016-10-26 14:17:11 -0500

    page 47

--- collection/views.py
+++ collection/views.py
@@ -1,20 +1,9 @@
 from django.shortcuts import render
+from collection.models import Thing
 
-# Create your views here.
-# def index(request):
-# 	#defining a variable
-# 	number = 9
-# 	thing = "Thing name"
-# 	#this is our new def def view
-# 	return render(request, 'index.html', {'number':number,})
 
 def index(request):
-	number = 6
-	# Dont forget the quotes because its a string,
-	# Not an interger
-	thing = "Thing name"
-	return render(request, 'index.html',{
-		'number':number,
-		#Donot forget to pass it in tand the last comma
-		'thing':thing,
+	things = Thing.objects.all()
+	return render(request, 'index.html', {
+		'things':things,
 	})
